93344d99b121128801904d5de277656c10fe2095
Zach Swanson <zach.swanson@theodysseyonline.com>
2018-01-31 15:16:05 -0500

    Initial commit

---
+++ src/app/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Starter - Vanilla</h1>
+      </div>
+    );
+  }
+
+};
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require("path");
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?/,
+                include: SRC_DIR,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015", "stage-2"]
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            importLoaders: 1,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: 'inline',
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
+module.exports = config;
